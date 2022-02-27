b9692bdab8dd225a1f8c98b60414491a9552a495
Tom Schneider <ric0.github@gmail.com>
2022-02-28 00:02:16 +0100

    feat: add page-not-found component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DartBoardComponent} from './dart-board/dart-board/dart-board.component';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {GameSelectionComponent} from './welcome-dartboard/initial-view/game-selection.component';
 
 const routes: Routes = [
   {path: '', component: GameSelectionComponent},
   {path: 'dartboard', component: DartBoardComponent},
+  {path: '**', component: PageNotFoundComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DartBoardModule } from './dart-board/dart-board.module';
-import { ScoreBoardModule } from './score-board/score-board.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { WelcomeDartboardModule } from './welcome-dartboard/welcome-dartboard.module';
-import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
-import { OvershotComponent } from './overshot/overshot.component';
-import { WonComponent } from './won/won.component';
-import { QuitQuestionComponent } from './quit-question/quit-question.component';
-import { MatDialogModule } from '@angular/material/dialog';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DartBoardModule} from './dart-board/dart-board.module';
+import {ScoreBoardModule} from './score-board/score-board.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {WelcomeDartboardModule} from './welcome-dartboard/welcome-dartboard.module';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import {OvershotComponent} from './overshot/overshot.component';
+import {WonComponent} from './won/won.component';
+import {QuitQuestionComponent} from './quit-question/quit-question.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {PageNotFoundModule} from "./page-not-found/page-not-found.module";
 
 
 @NgModule({
@@ -25,16 +26,19 @@ import { MatDialogModule } from '@angular/material/dialog';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    DartBoardModule,
     BrowserAnimationsModule,
-    MatSnackBarModule,
+
+    AppRoutingModule,
+
+    DartBoardModule,
     ScoreBoardModule,
-    MatToolbarModule,
     WelcomeDartboardModule,
+    PageNotFoundModule,
+
+    MatSnackBarModule,
+    MatToolbarModule,
     MatBottomSheetModule,
     MatDialogModule,
-
   ],
   providers: [],
   bootstrap: [AppComponent]
