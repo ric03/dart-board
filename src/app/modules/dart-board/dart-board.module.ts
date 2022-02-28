945c6a700cb466025db73bdc7719c9c467c5518e
Tom Schneider <ric0.github@gmail.com>
2022-02-28 23:43:54 +0100

    refactor: move input-button-row component into dart-board module

--- src/app/modules/dart-board/dart-board.module.ts
+++ src/app/modules/dart-board/dart-board.module.ts
@@ -1,26 +1,34 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatRippleModule} from "@angular/material/core";
 import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {AppRoutingModule} from '../../app-routing.module';
+import {MatTooltipModule} from "@angular/material/tooltip";
 import {GameSelectionModule} from '../game-selection/game-selection.module';
-import {InputModule} from '../input/input.module';
 import {ScoreBoardModule} from '../score-board/score-board.module';
 import {DartBoardComponent} from './dart-board/dart-board.component';
+import {InputButtonRowComponent} from "./input-button-row/input-button-row.component";
 
 
 @NgModule({
   declarations: [
     DartBoardComponent,
+    InputButtonRowComponent,
   ],
   imports: [
     CommonModule,
-    InputModule,
-    ScoreBoardModule,
-    MatToolbarModule,
-    GameSelectionModule,
+
+    MatButtonModule,
+    MatButtonToggleModule,
     MatMenuModule,
-    AppRoutingModule,
+    MatRippleModule,
+    MatToolbarModule,
+    MatTooltipModule,
+
+    GameSelectionModule,
+    ScoreBoardModule,
   ]
 })
 export class DartBoardModule {
