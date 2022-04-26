9f3b8bff59a776925c5aa8eb185df931f5173645
Tom Schneider <ric0.github@gmail.com>
2022-04-27 00:05:18 +0200

    fix: remove unused function

--- src/app/modules/cricket-board/cricket-board/cricket-board.component.ts
+++ src/app/modules/cricket-board/cricket-board/cricket-board.component.ts
@@ -1,23 +1,17 @@
-import { Component, HostListener, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import {Component, HostListener} from '@angular/core';
+import {environment} from 'src/environments/environment';
 
 @Component({
   selector: 'app-cricket-component',
   templateUrl: './cricket-board.component.html',
   styleUrls: ['./cricket-board.component.scss']
 })
-export class CricketBoardComponent{
+export class CricketBoardComponent {
   private env = environment;
 
   // TODO extract into service and enable conditionally?
   @HostListener('window:beforeunload', ['$event'])
   beforeUnloadHandler(event: any) {
-    if (this.env.production) {
-      event.preventDefault();
-    }
-  }
-  @HostListener('window:onload', ['$event'])
-  onload(event: any) {
     if (this.env.production) {
       event.preventDefault();
     }
