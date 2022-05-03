7adad1da167aba07cdfe194141677247e16a0af2
Tom Schneider <ric0.github@gmail.com>
2022-05-03 23:58:42 +0200

    refactor: assign empty value to mat-dialog-close directive, remove obsolete FIXME

--- src/app/dialogTemplates/quit-confirmation-dialog/quit-confirmation-dialog.component.ts
+++ src/app/dialogTemplates/quit-confirmation-dialog/quit-confirmation-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-quit-confirmation-dialog',
@@ -6,13 +6,11 @@ import { Component } from '@angular/core';
     <h1 mat-dialog-title>End</h1>
     <div mat-dialog-content>Do you want to play again?</div>
     <div mat-dialog-actions>
-      <button mat-button routerLink='' mat-dialog-close>Yes, let's play darts!</button>
-      <button mat-button mat-dialog-close>No, stay here and look around.</button>
+      <button mat-button mat-dialog-close="" routerLink="/">Yes, let's play darts!</button>
+      <button mat-button mat-dialog-close="">No, stay here and look around.</button>
     </div>
   `,
   styles: []
 })
 export class QuitConfirmationDialog {
 }
-
-// FIXME missing mat-button and mat-dialog-close attribute --> @Tom ist das noch nötig?
