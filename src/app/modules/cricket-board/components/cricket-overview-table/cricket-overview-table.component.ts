6572bf6f2cb2cecaaeaa9b432b297b9b523e38cb
JustCode7 <harrypottz85@gmail.com>
2025-01-18 21:52:50 +0100

    refactor: cricketview scroll into playersoverview

--- src/app/modules/cricket-board/components/cricket-overview-table/cricket-overview-table.component.ts
+++ src/app/modules/cricket-board/components/cricket-overview-table/cricket-overview-table.component.ts
@@ -14,5 +14,33 @@ export class CricketOverviewTableComponent {
 
   showAllPlayerdetails() {
     this.showPlayerDetails = !this.showPlayerDetails;
+    if (this.showPlayerDetails) {
+      this.waitForElm('#playercard0').then((elm) => {
+        // @ts-ignore
+        elm.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+      });
+    }
+
   }
+
+  waitForElm(selector: string) {
+    return new Promise(resolve => {
+      if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+      }
+
+      const observer = new MutationObserver(mutations => {
+        if (document.querySelector(selector)) {
+          observer.disconnect();
+          resolve(document.querySelector(selector));
+        }
+      });
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true
+      });
+    });
+  }
+
 }
